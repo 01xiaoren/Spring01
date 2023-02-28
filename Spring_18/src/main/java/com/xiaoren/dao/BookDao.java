e9d33cb9c59489bd22631eec95187f7d4d25efc8
xiaoren"git config --global user.email 2998024535@qq.comgit config --global user.name xiaoren <2998024535@qq.com>
2023-03-01 00:51:47 +0800

    aop的通知类型

---
+++ Spring_18/src/main/java/com/xiaoren/dao/BookDao.java
@@ -0,0 +1,7 @@
+package com.xiaoren.dao;
+
+public interface BookDao {
+    public void update();
+
+    public int select();
+}
---
+++ Spring_18/src/test/java/test.java
@@ -0,0 +1,13 @@
+import com.xiaoren.config.SpringConfig;
+import com.xiaoren.dao.BookDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class test {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfig.class);
+        BookDao bookDao = ctx.getBean(BookDao.class);
+        int num = bookDao.select();
+        System.out.println(num);
+    }
+}
